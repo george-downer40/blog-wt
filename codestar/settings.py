9be26ec058c44f47633d1b02a8d45d0ebf57e8eb
george-downer40 <george.downer40@gmail.com>
2024-02-13 09:46:10 +0000

    add generic post view

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-georgedowner40-blogwt-1n3bxd8p1xq.ws-eu108.gitpod.io',
                 '.herokuapp.com']
